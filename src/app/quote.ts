5d6013ee6247f5db15e95aab52ccfb54bde32157
Machuka Joy <machukajoy@gmail.com>
2022-04-13 00:59:34 +0300

    feature: quotes class: adds class to carry quotes

---
+++ src/app/quote.ts
@@ -0,0 +1,7 @@
+export class Quote {
+
+  constructor( public index:number, public author: string, public quote:string, public submitter: string, public datePost: Date){
+        
+
+  }
+}
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  
+  title = 'quote';
+
+  quotes: Quote [] = [
+
+    new Quote (1, 'Joy Machuka', "It's all a risk", 'Hellen Dale', new Date(2021, 5 , 22)),
+    new Quote (2, 'Precious Zawadi', "'The journey of 1000 miles continues with the second step.'", 'Noel Injaga', new Date(2021,12,25)),
+    new Quote (3, 'Kaavia James', "'We are going to need more wine.'", 'Gabrielle Union', new Date(2015,1,1)),
+  ]
 
   constructor() { }
 
