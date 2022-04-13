c63931e4017fe54b9ab4e4e35f62ebd8eee36297
Machuka Joy <machukajoy@gmail.com>
2022-04-13 13:06:23 +0300

    feature: imports submit and EventEmitter for submit function

--- src/app/quote-input-form/quote-input-form.component.ts
+++ src/app/quote-input-form/quote-input-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-input-form',
@@ -7,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteInputFormComponent implements OnInit {
 
-  newQuote = new Quotes(0, "", "", "", new Date());
+  newQuote = new Quote(0, "", "", "", new Date());
 
-  @Output() addQuote = new EventEmitter <Quotes>();
+  @Output() addQuote = new EventEmitter <Quote>();
 
   submitQuote(){
     this.addQuote.emit(this.newQuote);
-    this.newQuote = new Quotes(0, "", "", "", new Date());
+    this.newQuote = new Quote(0, "", "", "", new Date());
   }
   constructor() { }
 
