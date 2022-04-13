db619a562cc2a87485573a5f7e43714a947f42bc
Machuka Joy <machukajoy@gmail.com>
2022-04-13 11:35:31 +0300

    feature: adds date count pipe to count days

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let today: Date = new Date();
+    let dayWithNoTime: any = new Date (today.getFullYear(), today.getMonth(), today.getDate())
+    var difference = Math.abs(value - dayWithNoTime)
+    const secondsperDay = 86400;
+    var dateDifference = difference*0.001;
+    var dateCounter = Math.floor(dateDifference/secondsperDay);
+    
+    if (dateCounter >= 1 && value > dayWithNoTime){
+      return 0;
+    } else {
+      return dateCounter;
+    }
   }
 
 }
