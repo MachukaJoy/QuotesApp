23c4116fe7e0e5ec55ae555596333ae7d4c54228
Machuka Joy <machukajoy@gmail.com>
2022-04-13 07:13:39 +0300

    style: adds form and page styling

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,7 +12,7 @@ export class QuoteComponent implements OnInit {
 
   quotes: Quote [] = [
 
-    new Quote (1, 'Joy Machuka', "It's all a risk", 'Hellen Dale', new Date(2021, 5 , 22)),
+    new Quote (1, 'Joy Machuka', "'It's all a risk'", 'Hellen Dale', new Date(2021, 5 , 22)),
     new Quote (2, 'Precious Zawadi', "'The journey of 1000 miles continues with the second step.'", 'Noel Injaga', new Date(2021,12,25)),
     new Quote (3, 'Kaavia James', "'We are going to need more wine.'", 'Gabrielle Union', new Date(2015,1,1)),
   ]
