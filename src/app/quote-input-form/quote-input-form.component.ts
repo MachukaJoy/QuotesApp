59da53290546549dc5a6ab61804e09ca511be109
Machuka Joy <machukajoy@gmail.com>
2022-04-13 12:59:21 +0300

    feature: submit: add submit function

--- src/app/quote-input-form/quote-input-form.component.ts
+++ src/app/quote-input-form/quote-input-form.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteInputFormComponent implements OnInit {
 
+  newQuote = new Quotes(0, "", "", "", new Date());
+
+  @Output() addQuote = new EventEmitter <Quotes>();
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+    this.newQuote = new Quotes(0, "", "", "", new Date());
+  }
   constructor() { }
 
   ngOnInit(): void {
